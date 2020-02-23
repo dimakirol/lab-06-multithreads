9ac347ccca669fa806aed0ec91c68d3e5bc04203
dimakirol <60985893+dimakirol@users.noreply.github.com>
2020-02-23 15:47:41 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -10,31 +10,41 @@
 #include <cstdlib>
 #include <vector>
 #include <picosha2.h>
+#include <mutex>
 #define NUMBER_OF_THREADS std::thread::hardware_concurrency()
 
 void head() {
   auto func = []() //считать строки 7-32 просто необычно заданной функцией f
   {
-    std::string hex_str = ""; //в ней будет лежать хэш!!!!
+    std::mutex door_first;
+    door_first.lock();
+    auto hex_str = new string();
+    auto hash = new std::vector<unsigned char>(picosha2::k_digest_size);
+    door_first.unlock();
     do {
       //в строках 12-17 генерируем случайную строку от 5 до 50 символов
-      unsigned now = static_cast<unsigned int>(time(0));
+      std::mutex door_second;
+      door_second.lock();
+      auto now = new unsigned;
+      *now = static_cast<unsigned int>(time(0));
       uint32_t size = static_cast<char>(rand_r(&now) % 50 + 5);
-      std::string src_str = "";
+      auto src_str = new string();
+      door_second.unlock();
       for (uint32_t j = 0; j < size; ++j) {
-        src_str[j] = static_cast<char>(rand_r(&now) % 256);
+        (*src_str)[j] = static_cast<char>(rand_r(&now) % 256);
       }
       //в строках 19-22 считаем хэш для строки
-      std::vector<unsigned char> hash(picosha2::k_digest_size);
-      picosha2::hash256(src_str.begin(), src_str.end(), hash.begin(),
-                        hash.end());
-      hex_str = picosha2::bytes_to_hex_string(hash.begin(), hash.end());
+     
+      picosha2::hash256(src_str->begin(), src_str->end(), hash->begin(),
+                        hash->end());
+      *hex_str = picosha2::bytes_to_hex_string(hash->begin(), hash->end());
       //пока вывожу просто сиаутом id потока, сгенерированную строку и
       //полученный хэш, соответственно
       std::cout << "ID: " << std::this_thread::get_id();
-      std::cout << " string: '" << src_str.c_str();
-      std::cout << "' SHA= " << hex_str << std::endl;
-    } while (hex_str.rfind("0000") != 60);
+      std::cout << " string: '" << src_str->c_str();
+      std::cout << "' SHA= " << *hex_str << std::endl;
+    } while (hex_str->rfind("0000") != 60);
+    delete hex_str;
     //в данном месте искомый хэш найден и лежит в той же строке после
     //закрытия фигурной скобки на 32 строке данный поток закроется
     //так что лучше именно здесь логировать успешный результат
