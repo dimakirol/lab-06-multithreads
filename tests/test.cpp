2ae791dc15a8732766fffe9eaf4747e1cf5b6335
dimakirol <60985893+dimakirol@users.noreply.github.com>
2020-02-23 14:12:27 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,17 @@
 
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include "header.hpp"
+#include <gtest/gtest.h>
+
+TEST(Example, EmptyTest)
+{
+  head();
+  EXPECT_TRUE(true);
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
