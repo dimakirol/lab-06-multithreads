a0e37841ae6beaa1a1f957a2e765542c073ea160
kirill050 <kirilltikhonov050@gmail.com>
2020-02-25 23:09:52 +0300

    25.02.2020 23:09

--- include/header.hpp
+++ include/header.hpp
@@ -15,12 +15,16 @@
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/expressions/keyword.hpp>
+#include <boost/log/sinks.hpp>
 #include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/sources/severity_logger.hpp>
 #include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/core/null_deleter.hpp>
 
 namespace logging = boost::log;
 namespace src = boost::log::sources;
